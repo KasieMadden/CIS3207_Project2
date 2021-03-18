94a0db77236bd9c0fe492186b4a19a5ee57ee9f1
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-03-18 01:00:58 -0400

    all builtin commands working and tested

--- myshell.cpp
+++ myshell.cpp
@@ -1,8 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include "myshell.h"
 #include <stdio.h>
 #include <iostream>
 #include <unistd.h>
+#include <fstream>
 #include <sys/types.h>
 #include <dirent.h>
 #include <libgen.h>
@@ -12,16 +12,33 @@ using namespace std;
 
 extern char **environ;
 
+void directory(string dirname);
+void changeDir(char *dir);
+void clear();
+void environment(char *envp[]);
+void echo(char* str);
+void help(char* cmd);
+void Pause();
+int quit();
 
-void directory(char *dirname);
-string prompt = "";
+
+string prompt = " myshell>";
+string command = "";
+string command_line;
+
 
 int main(int argc, char *argv[],char *envp[]) {
 
-    directory("./");
+    directory(".");
+    environment(envp);
+
+    echo("\n echo echo echo echo....");
 
 
-}
+    while(true){ }//end of while
+
+
+}// end of main()*******************************************************
 
 
 //***********INTERNAL COMMANDS
@@ -35,7 +52,7 @@ int main(int argc, char *argv[],char *envp[]) {
 void changeDir(char *dir){
 
     if(dir == NULL){
-      cerr<< "Enter directory."<< std::endl;
+        cerr<< "Enter directory."<< std::endl;
     }
     else if(chdir(dir) == 0){
     }
@@ -44,27 +61,15 @@ void changeDir(char *dir){
 
 }
 
-//This function is used to clear the screen
-//Using regex to clear the screen
-//https://www.geeksforgeeks.org/clear-console-c-language
-void clear(){
-    int i;
-    for(i=0;i < 1000; i++) {
-        system("clr");
-    }
-
-}//end of clr
-
-
 //This function will show all the directories
 //if the pointer to directory is not null it wlll read thought
 //the directories print them and close. else error.
 //https://www.tutorialspoint.com/How-can-I-get-the-list-of-files-in-a-directory-using-C-Cplusplus
-void directory(char *dirname){
+void directory(string dirname){
 
     struct dirent *entry;
     DIR *dir;
-    if ((dir = opendir ("./")) == NULL) {
+    if ((dir = opendir(dirname.c_str())) == NULL) {
         perror ("Cannot open directory.");
         exit (1);
     }//end of if
@@ -81,48 +86,59 @@ void directory(char *dirname){
 
 }//end of dir
 
+
+//This function is used to clear the screen
+//Using regex to clear the screen
+//https://www.geeksforgeeks.org/clear-console-c-language
+void clear(){
+    int i;
+    for(i = 0;i < 1000; i++) {
+        system("clr");
+    }
+
+}//end of clr
+
+
 //This function will list all the environment strings*
 void environment(char *envp[]){
     int i;
 
-    for (i=0;i<sizeof(envp)-1;i++){
-       cout << envp[i] << endl;
+    for (i=0; envp[i]!= NULL; i++){
+        cout << envp[i] << endl;
     }
-
 
 }//end of environ
 
 //This function will show what is typed after the command  followed by a nm
+void echo(char str){
+    printf("%s",str);
+}//end of echo
+
 
 //help() runs though ifs  and prints what a command does
 //https://www.w3schools.com/cpp/cpp_files.asp
 void help(char* cmd){
     string helpText;
 
-    std::ifstream readFile("readme_doc.txt");
+    ifstream readFile("readme_doc.txt");
+
     while(getline(readFile, helpText)){
         cout<<helpText;
 
     }//end of while
 
-
 }//end of help
-
-
 
 
 //will pause shell until user hits enter
 void Pause(){
     cout << "Press Enter to Continue Program. ";
-    while(cin.get() != NULL){
+    while(cin.get() != 0){
         return ;
-    }
+    }//end while
 
 
 }//end of pause
-
-
-
 
 
 //will exit shell
